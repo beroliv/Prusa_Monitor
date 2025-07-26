befb0ca4d2d78ec462ddcc26d64a7eeab2f57fa3
Oliver <52623716+beroliv@users.noreply.github.com>
2025-07-26 12:42:23 +0200

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+CONTAINER_NAME="prusa-prusa_connect_rtsp-1"
+HOST_IP="192.168.0.61"
+
+while true; do
+    if ping -c 1 "$HOST_IP" > /dev/null 2>&1; then
+        echo "Host erreichbar."
+        if ! docker ps --format '{{.Names}}' | grep -q "$CONTAINER_NAME"; then
+            echo "Container nicht gefunden, starte ihn..."
+            docker start "$CONTAINER_NAME"
+        fi
+    else
+        echo "Host nicht erreichbar."
+        if docker ps --format '{{.Names}}' | grep -q "$CONTAINER_NAME"; then
+            echo "Container läuft, stoppe ihn..."
+            docker stop "$CONTAINER_NAME"
+        fi
+    fi
+    sleep 30
+done
